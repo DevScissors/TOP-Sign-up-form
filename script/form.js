0d338fbd283887881954023f2070cdf4849e337a
DevScissors <sean.scissors@gmail.com>
2026-05-10 18:12:08 -0700

    Updated validation to work properly and check isFormValid on input of password and email

--- script/form.js
+++ script/form.js
@@ -6,6 +6,8 @@ const includesUpperCaseLetter = document.querySelector(".includes-one-uppercase-
 const includesLowerCaseLetter = document.querySelector(".includes-one-lowercase-letter");
 const includesNumber = document.querySelector(".includes-one-number");
 const includesSpecialChar = document.querySelector(".includes-one-special-char");
+const emailInput = document.querySelector("#email");
+submitButton.disabled = true;
 
 function showPasswordError(message) {
     let passErrSpan = confirmPasswordInput.nextElementSibling;
@@ -29,6 +31,7 @@ function clearPasswordError() {
 function checkIfPasswordsMatch() {
     if (!confirmPasswordInput.value) {
         clearPasswordError();
+        submitButton.disabled = true;
         return;
     }
 
@@ -36,6 +39,7 @@ function checkIfPasswordsMatch() {
         showPasswordError("Passwords must match!");
     } else {
         clearPasswordError();
+        submitButton.disabled = !isFormValid();
     }
 }
 
@@ -61,6 +65,15 @@ const passwordRules = {
     hasMinLength: /.{8,}/,
 };
 
+function checkEmailIsValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+emailInput.addEventListener("input", () => {
+    submitButton.disabled = !isFormValid();
+    checkEmailIsValid(emailInput.value);
+})
+
 function validatePassword(password) {
     return {
         hasLowercase: passwordRules.hasLowercase.test(password),
@@ -79,7 +92,35 @@ passwordInput.addEventListener("input", (event) => {
     updateValidation("number", results.hasNumber);
     updateValidation("special", results.hasSpecial);
     updateValidation("length", results.hasMinLength);
+
+    // Update button state whenever password changes
+    submitButton.disabled = !isFormValid();
     checkIfPasswordsMatch();
 });
 
 confirmPasswordInput.addEventListener("input", checkIfPasswordsMatch);
+
+function isFormValid() {
+    const password = passwordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
+
+    let passwordsMatch = false;
+    if (password === confirmPassword) {
+        passwordsMatch = true;
+    }
+
+    const email = emailInput.value;
+    const emailValid = checkEmailIsValid(email);
+    console.log(emailValid);
+
+    const validatePassObj = validatePassword(password);
+    const passwordValid = Object.values(validatePassObj).every(req => req);
+
+    let validationPasses = false;
+    // Check all requirements are met AND passwords match
+    if (passwordsMatch && passwordValid && emailValid) {
+        validationPasses = true;
+    }
+
+    return passwordValid && validationPasses;
+}
